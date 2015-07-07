9a13e8b6e5fd25724910b5dfc2aa6df2adbea74e
Casey Kolderup <casey.kolderup@voxmedia.com>
2015-07-07 15:03:01 -0400

    turn off 4th of July

--- app.rb
+++ app.rb
@@ -19,37 +19,19 @@ end.parse!
 @departments = File.readlines("department.txt")
 @titles = File.readlines("title.txt")
 
-@independence = %w{Fireworks Flag Independence Bonfire Baton-Twirl Parade Grill Cookout Stars Stripes Constitution United Democracy Explosion Sparkler Summer Firecracker July Patriot Picnic Hamburger Hotdog Streamer Sunburn Poolparty Union Victory Beer Watermelon Bang Boom Crackle Pie Spangle Liberty State Congressman Senator President Liberty Justice Baseball Taxation Yankee Colony Allegiance Freedom}
-
-def first_name
-  if rand(1..100) >= 97
-    @independence.sample
-  else
-    @firsts.sample.chomp
-  end
-end
-
-def surname
-  if rand(1..100) >= 25
-    @independence.sample
-  else
-    @surnames.sample.chomp
-  end
-end
-
 def computer_company
-  company_front = %w[Elec Inter Macro Globo Hyper Infra] + @independence
+  company_front = %w[Elec Inter Macro Globo Hyper Infra]
   company_back = %w[tron node trode soft systems ]
   "#{company_front.sample}#{company_back.sample}"
 end
 
 def named_firm
-  "#{surname} & #{surname}"
+  "#{@surnames.sample.chomp} & #{@surnames.sample.chomp}"
 end
 
 def dumb
   first = %w[Global Syndicated Amalgamated Professional International Distributed]
-  last = %w[Meats Futures Industries Metals Investments Logging Infrastructure Instruction Development Research Systems] + @independence
+  last = %w[Meats Futures Industries Metals Investments Logging Infrastructure Instruction Development Research Systems]
   designator = %w[Inc Corp LLC]
   "#{first.sample} #{last.sample} #{designator.sample}"
 end
@@ -66,10 +48,6 @@ def image(name, title, company)
   file.rewind
   bin = File.open(file,'r'){ |f| f.read }
   image = Image.from_blob(bin).first
-
-  image = image.opaque('#55FFFF', 'red')
-  image = image.opaque('#FF55FF', 'blue')
-
   append = Image.new(750, 500) do
     self.background_color = 'black'
   end
@@ -115,14 +93,14 @@ def image(name, title, company)
   file
 end
 
-company = %w[named_firm computer_company dumb] #acronym]
+company = %w[named_firm computer_company dumb acronym]
 
 @name = nil
 @title = nil
 @company = nil
 length = 141
 while length > 140 do
-    @name = "#{first_name} #{surname}"
+    @name = "#{@firsts.sample.chomp} #{@surnames.sample.chomp}"
     @title = "#{@ranks.sample.chomp} #{@departments.sample.chomp} #{@titles.sample.chomp}"
     @company = "#{send(company.sample)}"
     out = "#{@name}, #{@title} at #{@company}"
